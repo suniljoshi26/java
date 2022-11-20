7a63549f5505ca878b5eab73650c559e16df3960
Sunil Joshi <joshisunil2601@gmail.com>
2022-11-20 18:04:30 +0530

    Override

--- src/Elephant.java
+++ src/Elephant.java
@@ -2,5 +2,9 @@ public class Elephant extends Animal {
     public void sundhUthao() {
         System.out.println("sundhUthao");
     }
-    
+
+    @Override
+    public void walk() {
+        System.out.println("ggfyuyfuy");
+    }
 }
